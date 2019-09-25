69a0d8743ff54279e554af761d99831eb503dbea
unknown <825633757@qq.com>
2019-09-25 15:30:51 +0800

    页面修改

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,62 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div style="height: 100%">
+    <el-container style="height: 100%">
+      <el-header style="padding: 0 0 0 0;">
+        <HeadIndex></HeadIndex>
+      </el-header>
+      <el-container>
+        <el-aside width="400px">
+          <MenuIndex></MenuIndex>
+        </el-aside>
+        <el-container>
+          <el-main>
+            <router-view></router-view>
+          </el-main>
+          <el-footer>Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
   </div>
 </template>
-
-<style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import MenuIndex from "./views/MenuIndex";
+import HeadIndex from "./views/HeadIndex";
+export default {
+  name: "app",
+  components: {
+    MenuIndex: MenuIndex,
+    HeadIndex: HeadIndex
   }
+};
+</script>
+<style lang="less">
+.el-header,
+.el-footer {
+  width: 100%;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
 }
 </style>
